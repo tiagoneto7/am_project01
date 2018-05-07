aaef580f4c33cc34f2339de2e3aa841ed0410340
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2018-05-07 01:43:52 +0100

    Adicionei sistema de stamina
    Criei uma nova entidade Vitals, que irá conter quaisqueres valor vitais do player (de momento só tem stamina, mais tarde poderá te informação de stun, etc).
    
    Esta entidade tem um método que dependendo do nível de resistência do player, restaura a sua stamina.
    
    Adicionei tambem um bool na class Player que determina se o palyer está a sprintar ou não.

--- jogo-base/Assets/Scripts/entities/Player.cs
+++ jogo-base/Assets/Scripts/entities/Player.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private Character character;
+    private Vitals vitals;
+
+    private bool sprinting;
+
+    public Player() {
+        this.vitals = new Vitals();
+        this.sprinting = false;
+    }
     
 	void Start () {
         this.character = new Scout();
     }
 
     void Update () {
+
+        if (sprinting)
+            Debug.Log("Alterar velocidade aqui");
+        else
+            vitals.restoreStamina(character.getLevel(Skill.Resistance));
 	}
+
 }
---
+++ jogo-base/Assets/Scripts/entities/Vitals.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vitals : MonoBehaviour {
+
+    /* O limite máximo de stamina (era melhor mante-lo entre os limites de percentagem 0-100) */
+    private const int MAX_STAMINA = 100;
+
+    /* O multiplier a adicionar por cada game tick, quanto maior, mais rápido a stamina regenera */
+    private const float staminaMultiplier = 0.8f;
+
+    private float stamina;
+
+    public Vitals()
+    {
+        this.stamina = 0;
+    }
+
+    /* Restaura a stamina todos os game ticks (sobe a stamina por uma pequena quantidade) */
+    public void restoreStamina(int resistance)
+    {
+        float amount = resistance * staminaMultiplier * Time.deltaTime;
+
+        /* ignorar quantidades negativas ou zero */
+        if (amount <= 0)
+            return;
+
+        /* limitar a stamina a 100 */
+        if (amount + stamina > MAX_STAMINA)
+            stamina = MAX_STAMINA;
+        else
+            stamina += amount;
+    }
+}
